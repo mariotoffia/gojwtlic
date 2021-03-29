bd82e0941ed838c799973a61ea43cda50b14704b
Mario Toffia <mario.toffia@gmail.com>
2021-03-29 14:29:22 +0200

    wrapper for PEP

--- license/licpol/pep.go
+++ license/licpol/pep.go
@@ -10,43 +10,12 @@ import (
 type PEPInvoke interface {
 	GetParams() []interface{}
 	GetMethod() []string
-	GetFunction() interface{}
+	GetFunction() reflect.Value
 }
 
 type PEPReturn interface {
 	PEPInvoke
 	GetReturn() []interface{}
-}
-
-// pepmsg is a struct that implements `PEPInvoke` and `PEPReturn`
-//
-// In this way the implementation do not need to instantiate two structs
-// when invoke + return.
-type pepmsg struct {
-	params   []interface{}
-	method   []string
-	function interface{}
-	ret      []interface{}
-}
-
-func (pmsg *pepmsg) GetParams() []interface{} {
-	return pmsg.params
-}
-func (pmsg *pepmsg) GetMethod() []string {
-	return pmsg.method
-}
-func (pmsg *pepmsg) GetFunction() interface{} {
-	return pmsg.function
-}
-func (pmsg *pepmsg) GetReturn() []interface{} {
-	return pmsg.ret
-}
-
-type PolicyEnforcementPoint interface {
-	// Invoke will enforce a invocation that is about to happen
-	Invoke(f interface{}, method string, prm ...interface{}) PEPInvoke
-	// Return will enforce a return from a previous PEP invoke request.
-	Return(invoke PEPInvoke, prm ...interface{}) PEPReturn
 }
 
 // PEPRegistration is a single entry registration for doing a PE
@@ -57,8 +26,47 @@ type PEPRegistration struct {
 	Parameters []string
 	Returns    []string
 	Function   interface{}
-	t          reflect.Type
+	v          reflect.Value
 	method     []string
+	wrapper    interface{}
+}
+
+// pepmsg is a struct that implements `PEPInvoke` and `PEPReturn`
+//
+// In this way the implementation do not need to instantiate two structs
+// when invoke + return.
+type pepmsg struct {
+	reg    *PEPRegistration
+	params []interface{}
+	ret    []interface{}
+}
+
+func (pmsg *pepmsg) GetParams() []interface{} {
+	return pmsg.params
+}
+func (pmsg *pepmsg) GetMethod() []string {
+	return pmsg.reg.method
+}
+func (pmsg *pepmsg) GetFunction() reflect.Value {
+	return pmsg.reg.v
+}
+func (pmsg *pepmsg) GetReturn() []interface{} {
+	return pmsg.ret
+}
+
+type PolicyEnforcementPoint interface {
+	// CheckInvoke will check if it can be invoked or not.
+	CheckInvoke(method string, prm ...interface{}) PEPInvoke
+	// CheckReturn checks if return values is possible.
+	//
+	// The _invoke_ parameter is the returned parameter from `CheckInvoke`
+	CheckReturn(invoke PEPInvoke, out ...interface{}) PEPReturn
+	// Wrapper returns the function wrapper that will invoke the function and do
+	// all PEP processing.
+	//
+	// CAUTION: This function uses reflection and is *much slower* than invoking
+	// the function directly!
+	Wrapper(method string) interface{}
 }
 
 type PEP struct {
@@ -67,6 +75,15 @@ type PEP struct {
 
 // NewPolicyEnforcementPoint creates a new _PEP_ which supports the provided functions.
 func NewPolicyEnforcementPoint(functions map[string]PEPRegistration) *PEP {
+	return NewPolicyEnforcementPointWithWrapper(functions, false)
+}
+
+// NewPolicyEnforcementPointWithWrapper creates a new _PEP_ which supports the provided functions.
+//
+// If _createWrapper_ is set to `true`, it will create a function wrapper as convenience method.
+func NewPolicyEnforcementPointWithWrapper(
+	functions map[string]PEPRegistration,
+	createWrapper bool) *PEP {
 
 	pep := &PEP{
 		funcs: map[string]PEPRegistration{},
@@ -80,7 +97,7 @@ func NewPolicyEnforcementPoint(functions map[string]PEPRegistration) *PEP {
 			panic(fmt.Sprintf("expects a function, got %T for method: %s", registration, method))
 		}
 
-		registration.t = rf
+		registration.v = reflect.ValueOf(registration.Function)
 
 		if rf.NumIn() != len(registration.Parameters) {
 
@@ -105,30 +122,76 @@ func NewPolicyEnforcementPoint(functions map[string]PEPRegistration) *PEP {
 		}
 
 		registration.method = strings.Split(method, "/")
-		pep.funcs[method] = registration
 
+		if createWrapper {
+
+			registration.wrapper = reflect.MakeFunc(rf, func(in []reflect.Value) []reflect.Value {
+
+				prm := make([]interface{}, len(in))
+
+				for i := range in {
+					prm[i] = in[i].Interface()
+				}
+
+				result := pep.CheckInvoke(method, prm...)
+
+				// TODO: handle rejection or filtering of parameters
+
+				// TODO: If partial resolved policy, the function need to accept
+				// TODO: PEPInvoke as second param (CbContext as first param).
+				// TODO: Add ast to PEPInvoke interface for GetPartial()...
+
+				out := registration.v.Call(in)
+
+				// TODO: Process and - CheckReturn()
+				outprm := make([]interface{}, len(out))
+
+				for i := range out {
+					outprm[i] = out[i].Interface()
+				}
+
+				/*ret := */
+				pep.CheckReturn(result, outprm...)
+				// TODO: Extract return arguments and return them.
+
+				// TODO: it should return ret instead
+				return out
+			}).Interface()
+
+			pep.funcs[method] = registration
+		}
 	}
 
 	return pep
 }
 
-func (pep *PEP) Invoke(f interface{}, method string, prm ...interface{}) PEPInvoke {
+func (pep *PEP) Wrapper(method string) interface{} {
+
+	if registration, ok := pep.funcs[method]; ok {
+		return registration.wrapper
+	}
+
+	panic(fmt.Sprintf("not part of this PEP, method: %s", method))
+}
+
+// CheckInvoke will check if it can be invoked or not.
+func (pep *PEP) CheckInvoke(method string, prm ...interface{}) PEPInvoke {
 
 	registration := pep.funcs[method]
 
-	if registration.t == nil {
-		panic(fmt.Sprintf("not part of this PEP, method: %s and function:%T", method, f))
+	if registration.Parameters == nil {
+		panic(fmt.Sprintf("not part of this PEP, method: %s", method))
 	}
 
 	pmsg := &pepmsg{
-		function: f,
-		method:   registration.method,
-		params:   prm,
+		reg:    &registration,
+		params: prm,
+		ret:    nil,
 	}
 
 	msg := PDPMessage{
 		Type:   "invoke",
-		Method: pmsg.method,
+		Method: pmsg.reg.method,
 		Body:   map[string]interface{}{},
 	}
 
@@ -144,5 +207,13 @@ func (pep *PEP) Invoke(f interface{}, method string, prm ...interface{}) PEPInvo
 	fmt.Println("Feeding JSON to PDP")
 	fmt.Println(string(json))
 
+	// TODO: feed to PDP and process the result into pmsg
+
 	return pmsg
+
+}
+
+func (pep *PEP) CheckReturn(invoke PEPInvoke, out ...interface{}) PEPReturn {
+	// TODO: Implement me!
+	return invoke.(*pepmsg)
 }
--- license/licpol/reflect_test.go
+++ license/licpol/reflect_test.go
@@ -5,22 +5,86 @@ import (
 	"testing"
 )
 
-func MyFunc(name, dir string) string {
-	return fmt.Sprintf("%s-%s", name, dir)
+type MyStruct struct {
+	Name   string
+	Age    int
+	secret string
 }
 
-func TestReflectiveParamsExtraction(t *testing.T) {
+func MyFunc2(ms MyStruct) string {
 
-	pdp := NewPolicyEnforcementPoint(
+	v := fmt.Sprintf("%s, %d (%s)", ms.Name, ms.Age, ms.secret)
+	fmt.Println(v)
+	return v
+
+}
+
+func MyFunc(name, dir string) string {
+
+	v := fmt.Sprintf("%s-%s", name, dir)
+	fmt.Println(v)
+	return v
+
+}
+
+func TestReflectivePrimitiveParams(t *testing.T) {
+
+	pdp := NewPolicyEnforcementPointWithWrapper(
 		map[string]PEPRegistration{
 			"path/to/MyFunc": {
 				Function:   MyFunc,
 				Parameters: []string{"name", "dir"},
 				Returns:    []string{"output"},
 			},
-		})
+		}, true /*createWrapper*/)
 
-	res := pdp.Invoke(MyFunc, "path/to/MyFunc", "hello", "world")
+	res := pdp.Wrapper("path/to/MyFunc").(func(name, dir string) string)(
+		"kalle", "kobra",
+	)
 
-	fmt.Println(res)
+	fmt.Printf("result: %s\n", res)
+}
+
+func TestReflectiveStructParam(t *testing.T) {
+
+	pdp := NewPolicyEnforcementPointWithWrapper(
+		map[string]PEPRegistration{
+			"path/to/MyFunc2": {
+				Function:   MyFunc2,
+				Parameters: []string{"ms"},
+				Returns:    []string{"output"},
+			},
+		}, true /*createWrapper*/)
+
+	res := pdp.Wrapper("path/to/MyFunc2").(func(ms MyStruct) string)(
+		MyStruct{
+			Name:   "Nisse",
+			Age:    17,
+			secret: "shh"},
+	)
+
+	fmt.Printf("result: %s\n", res)
+}
+
+func BenchmarkWapInvoke(t *testing.B) {
+
+	f := func(ms MyStruct) string { return "" }
+
+	pdp := NewPolicyEnforcementPointWithWrapper(
+		map[string]PEPRegistration{
+			"path/to/MyFunc2": {
+				Function:   f,
+				Parameters: []string{"ms"},
+				Returns:    []string{"output"},
+			},
+		}, true /*createWrapper*/)
+
+	prm := MyStruct{Name: "Nisse", Age: 17, secret: "shh"}
+	method := "path/to/MyFunc2"
+
+	t.ResetTimer()
+
+	for i := 0; i < t.N; i++ {
+		pdp.Wrapper(method).(func(ms MyStruct) string)(prm)
+	}
 }
